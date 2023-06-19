e5a7fbf90611d17c93adaf6240837c66c8757af3
shin100zzz <nhnghia2406@gmail.com>
2023-06-20 00:39:57 +0700

    create new brank

---
+++ testCases/common/TC_03.java
@@ -0,0 +1,19 @@
+package common;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.AfterClass;
+
+public class TC_03 {
+  @Test
+  public void f() {
+  }
+  @BeforeClass
+  public void beforeClass() {
+  }
+
+  @AfterClass
+  public void afterClass() {
+  }
+
+}
