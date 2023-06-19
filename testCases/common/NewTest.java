25924b84733cedc9459eae7473041f7848f77430
shin100zzz <nhnghia2406@gmail.com>
2023-06-20 00:32:52 +0700

    initial source code

---
+++ testCases/common/NewTest.java
@@ -0,0 +1,9 @@
+package common;
+
+import org.testng.annotations.Test;
+
+public class NewTest {
+  @Test
+  public void f() {
+  }
+}
---
+++ testCases/common/TC01_Login.java
@@ -0,0 +1,24 @@
+package common;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.AfterClass;
+
+public class TC01_Login {
+	
+
+  @BeforeClass
+  public void beforeClass() {
+  }	 
+	 
+  @Test
+  public void TC_01() {
+  }
+ 
+
+  @AfterClass
+  public void afterClass() {
+  }
+
+}
+ 
