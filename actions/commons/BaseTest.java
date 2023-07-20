dc11e0b9d5bac09e835c25d34413502e54e6699f
shin100zzz <nhnghia2406@gmail.com>
2023-07-20 07:37:59 +0700

    DriverManager for multiple browser

--- actions/commons/BaseTest.java
+++ actions/commons/BaseTest.java
@@ -5,6 +5,8 @@ import org.openqa.selenium.chrome.ChromeDriver;
 //import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 public class BaseTest {
 	private WebDriver driver;
 	private String projectPath = System.getProperty("user.dir");
@@ -14,12 +16,18 @@ public class BaseTest {
 		if (osName.contains("Windows")) {
 			System.setProperty("webdriver.gecko.driver", projectPath + "\\browserDrivers\\chromedriver.exe");
 //			System.setProperty("webdriver.chrome.driver", projectPath + "\\browserDrivers\\chromedriver.exe");		
-		} else {
+			WebDriverManager.firefoxdriver().setup();
+		} 
+		else {
 			if(browserName.equals("firefox")) {
-				System.setProperty("webdriver.gecko.driver", projectPath + "/browserDrivers/geckodriver");
+//				System.setProperty("webdriver.gecko.driver", projectPath + "/browserDrivers/geckodriver");
+				WebDriverManager.firefoxdriver().setup();
 				driver = new FirefoxDriver();
-			}else if(browserName.equals("chrome")) {
-				System.setProperty("webdriver.chrome.driver",projectPath + "/browserDrivers/chromedriver");
+			}
+			else 
+				if(browserName.equals("chrome")) {
+//				System.setProperty("webdriver.chrome.driver",projectPath + "/browserDrivers/chromedriver");
+				WebDriverManager.chromedriver().driverVersion("114.0.5735.16").setup();
 				driver = new ChromeDriver();
 			}
 //			else if(browserName.equals("edge")) {
